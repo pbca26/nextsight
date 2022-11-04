108f3d8fc2abed9c2923cadbcfaf93217c91393a
pbca26 <pbca26@gmail.com>
2022-11-04 18:04:20 +0300

    add styling, main app comp

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import './css/main.css';
+import './css/common.min.css';
+import './css/overrides.css';
+import './css/branding.css';
+import './css/custom.css';
+import './css/cards.css';
+import './css/stats.css';
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
